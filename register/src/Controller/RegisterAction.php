ae90ca305e61a810a76ddb5fa6806ae01c0bf5d3
Richard Rodriguez <richardrodriguez21@gmail.com>
2025-05-12 13:34:14 +0200

    feat(register): enhance messenger configuration for RabbitMQ integration and update environment variables

--- register/src/Controller/RegisterAction.php
+++ register/src/Controller/RegisterAction.php
@@ -6,12 +6,19 @@ namespace App\Controller;
 use App\Http\DTO\RegisterRequest;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
-
-final class RegisterAction
+use App\Service\RegisterActionService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+class RegisterAction extends AbstractController
 {
+
+  public function __construct(private readonly RegisterActionService $registerActionService){
+
+  }
+
     #[Route('/api/register', name: 'api_register', methods: ['POST'])]
     public function __invoke(RegisterRequest $registerRequest): JsonResponse
     {
+      $this->registerActionService->__invoke($registerRequest->getName(), $registerRequest->getEmail());
       return new JsonResponse($registerRequest->getEmail() . ' ' . $registerRequest->getName());
     }
 }
